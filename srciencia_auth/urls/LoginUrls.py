9cf98bc45cb86232e001dd08d46e3be52e18bf3c
Potiguaras <ifrn.potiguaras@gmail.com>
2024-10-24 19:01:51 -0300

    forgot_password

--- srciencia_auth/urls/LoginUrls.py
+++ srciencia_auth/urls/LoginUrls.py
@@ -5,4 +5,5 @@ from srciencia_auth.views.CadastroView import register
 urlpatterns = [
     path("login/", login_view, name='login'),
     path("cadastro/", register, name='cadastro'),
+    path("esqueci_minha_senha/", login_view, name='esqueci_minha_senha')
 ]
