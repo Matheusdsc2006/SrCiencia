fa85b4aa7c31fe5706522c784bbdec4ceb1b2c13
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2024-10-25 17:34:33 -0300

    fix: bugs SMTP

--- srciencia_auth/urls/LoginUrls.py
+++ srciencia_auth/urls/LoginUrls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from srciencia_auth.views.LoginView import login_view
+from srciencia_auth.views.LoginView import *
 from srciencia_auth.views.CadastroView import register
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("login/", login_view, name='login'),
     path("cadastro/", register, name='cadastro'),
-    path('password_reset_form', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password_reset_form'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', password_reset_done, name="password_reset_done"),
 ]
