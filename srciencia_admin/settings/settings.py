833230b96d2aae5a21a5b5caf91387a609e44be7
Potiguaras <ifrn.potiguaras@gmail.com>
2024-10-25 15:02:53 -0300

    ajustar

--- srciencia_admin/settings/settings.py
+++ srciencia_admin/settings/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import ssl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- srciencia_auth/urls/LoginUrls.py
+++ srciencia_auth/urls/LoginUrls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("login/", login_view, name='login'),
     path("cadastro/", register, name='cadastro'),
-    path('password_reset_form.html', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password_reset_form.html'),
+    path('password_reset_form', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password_reset_form'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
