086c100c8612b04385965ccbc3a0c0e52e93324a
Potiguaras <ifrn.potiguaras@gmail.com>
2024-12-28 23:18:03 -0300

    salvando

--- srciencia_core/views/Visualizar_alunosView.py
+++ srciencia_core/views/Visualizar_alunosView.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from srciencia_core.models import Turma
+from django.http import JsonResponse
 
 
 def alunos_turma_view(request, turma_id):
     turma = get_object_or_404(Turma, id=turma_id)
-    alunos = turma.aluno_set.all()
-    return render(request, 'alunos_turma.html', {'turma': turma, 'alunos': alunos})
-
-
-
-
-
-
-
-
+    alunos = turma.aluno_set.all().values("nome", "email")
+    return JsonResponse({"success": True, "alunos": list(alunos)})
